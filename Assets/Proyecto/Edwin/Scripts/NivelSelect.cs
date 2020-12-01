1771a47b6ce066fae8daaca06bb43981ae9a7d09
edwinsaavedra99 <40569878+edwinsaavedra99@users.noreply.github.com>
2020-12-01 15:22:55 -0500

    design UI

---
+++ Assets/Proyecto/Edwin/Scripts/NivelSelect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class NivelSelect : MonoBehaviour, IPointerClickHandler
+{
+    public string nameLevel = "";    
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Application.LoadLevel(nameLevel);
+    }
+}
