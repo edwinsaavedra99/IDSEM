c1f4d77e0f6e1a22950423c3cd601a3d65d3c2f9
Luis Angel Lorenzo Quilla <louysotopo123@gmail.com>
2020-12-01 20:36:12 -0500

    Avance de enemigo final

---
+++ Assets/Proyecto/Luis/Scripts/ScripsGirl/SimpleGirlMovement.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class SimpleGirlMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    //public Action actualAction = Action.Idle;
+    //int actualAnim = (int)Action.Idle;
+    private const float SPEED = 5f;
+    //public Animator animator;
+    public Text lifeDisplayText;
+    public Text enemyDisplayText;
+    public Text messageDisplayText; //food
+    //public Text foodDislayText; //food
+    public int lifepoints;
+    public int enemypointsinit;//food
+    public Slider lifeSlider;
+    public Slider enemySlider;
+    public int enemyDamage;
+    public GameObject winDisplay;
+    public GameObject loseDisplay;
+    void Start()
+    {
+        this.lifeSlider.minValue = 0;
+        this.lifeSlider.maxValue = lifepoints;
+        this.lifeSlider.value = lifepoints;
+        this.lifeDisplayText.text = "" + lifepoints;
+
+        this.enemySlider.minValue = 0;
+        this.enemySlider.maxValue = enemypointsinit;
+        this.enemySlider.value = enemypointsinit;
+        this.enemyDisplayText.text = ""+enemypointsinit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        if (lifepoints <= 0)
+        {
+            Debug.Log("Perdiste");
+            //this.animator.SetTrigger("sleepGirl");
+            loseDisplay.SetActive(true);
+        }
+        if (enemypointsinit <= 0) {
+            Debug.Log("Ganaste");
+            this.winDisplay.SetActive(true);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        float moveX = 0f;
+        float moveY = 0f;
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            //this.animator.SetTrigger("atackGirl");
+            //moveY = +1f;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            //this.animator.SetTrigger("atackGirl");
+            //moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = Quaternion.Euler(0,180,0);
+            this.animator.SetTrigger("isWalking");
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = Quaternion.Euler(0,0,0);
+            this.animator.SetTrigger("isWalking");
+            moveX = +1f;
+        }
+        
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+
+        transform.position += moveDir * SPEED * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Enemy" )
+        {
+            Debug.Log("muere como enemigo");
+             this.lifepoints = this.lifepoints - this.enemyDamage;
+            this.lifeDisplayText.text = "" + lifepoints;
+            this.lifeSlider.value = lifepoints;
+            Destroy(collider.gameObject);
+        }
+        if(collider.tag == "Message")
+        {
+            Debug.Log("mensanje");
+            this.messageDisplayText.text = "";
+        }
+        
+        
+    }
+}
---
+++ Assets/Proyecto/Luis/Scripts/ScriptEnemy/EnemyBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    public float visionRadious;
+    
+    private float variable;
+    public float speed;
+    public GameObject player;
+    Vector3 initialPosition; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //player = GameObject.FindGameObjectWithTag("Player");
+        initialPosition = transform.position;
+        this.variable = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 target = initialPosition;
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < visionRadious)
+        {
+            target = player.transform.position;
+        }
+        if( transform.position.x >= variable ) {
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }else{
+           transform.rotation = Quaternion.Euler(0,180,0);
+        }
+        this.variable = transform.position.x;
+
+        float fixedSpeed = speed*Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target, fixedSpeed);
+        Debug.DrawLine(transform.position, target, Color.green); 
+
+    }
+}
